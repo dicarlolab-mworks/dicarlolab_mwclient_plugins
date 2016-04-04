dicarlolab-mworks/dicarlolab_mwclient_plugins

commit c162edb6e3a111e6808fc670512c751c2f3ed936

Updates for Datum changes

CalibratorWindow/MWCalibratorWindowController.m
@@ -86,7 +86,7 @@
 					if(cal_announce.getElement(CALIBRATOR_ACTION) == CALIBRATOR_ACTION_UPDATE_PARAMS) {
 						Datum calibratorNameElement(cal_announce.getElement(CALIBRATOR_NAME));
 						
-						NSString *calibratorName = [NSString stringWithCString:calibratorNameElement.getString()
+						NSString *calibratorName = [NSString stringWithCString:calibratorNameElement.getString().c_str()
 																	  encoding:NSASCIIStringEncoding];
 						
 						Datum hParams(cal_announce.getElement(CALIBRATOR_PARAMS_H));
@@ -277,7 +277,7 @@
 			
 			Datum hParams(M_LIST, [cr getNHParameters]);
 			
-			for(int i=0; i<hParams.getMaxElements(); ++i) {
+			for(int i=0; i<hParams.getNElements(); ++i) {
 				Datum param(M_FLOAT, [cr getHParameter:i]);
 				hParams.setElement(i,param);
 			}
@@ -286,7 +286,7 @@
 			
 			Datum vParams(M_LIST, [cr getNVParameters]);
 			
-			for(int i=0; i<vParams.getMaxElements(); ++i) {
+			for(int i=0; i<vParams.getNElements(); ++i) {
 				Datum param(M_FLOAT, [cr getVParameter:i]);
 				vParams.setElement(i,param);
 			}

EyeWindow/MWPlotView.m
@@ -503,8 +503,8 @@ static void removeExpiredSamples(NSMutableArray *samples, NSTimeInterval cutoffT
                     width_x_data = width_y_data = stm_announce.getElement("width");
                 }
                 
-                NSString* stm_type = @(type_data.getString());
-                NSString* stm_name = @(name_data.getString());
+                NSString* stm_type = @(type_data.getString().c_str());
+                NSString* stm_name = @(name_data.getString().c_str());
                 float stm_pos_x = pos_x_data.getFloat();
                 float stm_pos_y = pos_y_data.getFloat();
                 float stm_width_x = width_x_data.getFloat();

MATLABWindow/MATLABInterface.m
@@ -100,7 +100,7 @@
 					withCodec:(Datum *)codec {
 	
 	[interfaceLock lock];
-	mxArray *codecStruct = getCodec(codec->getScarabDatum());
+	mxArray *codecStruct = getCodec(codec->toScarabDatum().get());
 	int nevents = [dataEventList count];
 	
 	mxArray *data_struct = [self createTopLevelDataStructure:STREAM];

MATLABWindow/MWMATLABWindowController.m
@@ -324,9 +324,10 @@
 		[vl clear];
 		
 		// determine max code number of codec
+        auto scarabCodec = new_codec->toScarabDatum();
 		int max_key = -1;
 		for(int i=0; i< new_codec->getNElements(); ++i) {
-			max_key = MAX(new_codec->getScarabDatum()->data.dict->keys[i]->data.integer, max_key);
+			max_key = MAX(scarabCodec.get()->data.dict->keys[i]->data.integer, max_key);
 		}
 		
 		
@@ -334,7 +335,7 @@
 			Datum codec_entry(new_codec->getElement(Datum(M_INTEGER, i)));
 			
 			if(!codec_entry.isUndefined()) {
-				NSString *variableName = [NSString stringWithCString:codec_entry.getElement("tagname").getString() 
+				NSString *variableName = [NSString stringWithCString:codec_entry.getElement("tagname").getString().c_str()
 															encoding:NSASCIIStringEncoding];
 				MWVarEntry *var = [[MWVarEntry alloc] initWithName:variableName];
 				

commit ec0d6d4431fcb71072ef5514645b3a8d10cebbdc

Fixed accidental breakage of calibrator window in commit c162edb

CalibratorWindow/MWCalibratorWindowController.m
@@ -277,7 +277,7 @@
 			
 			Datum hParams(M_LIST, [cr getNHParameters]);
 			
-			for(int i=0; i<hParams.getNElements(); ++i) {
+			for(int i=0; i<[cr getNHParameters]; ++i) {
 				Datum param(M_FLOAT, [cr getHParameter:i]);
 				hParams.setElement(i,param);
 			}
@@ -286,7 +286,7 @@
 			
 			Datum vParams(M_LIST, [cr getNVParameters]);
 			
-			for(int i=0; i<vParams.getNElements(); ++i) {
+			for(int i=0; i<[cr getNVParameters]; ++i) {
 				Datum param(M_FLOAT, [cr getVParameter:i]);
 				vParams.setElement(i,param);
 			}